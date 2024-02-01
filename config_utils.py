58ef38d97923b896e619d961a357890f43a9fd8d
adimulya <1kartiyasa@gmail.com>
2024-02-01 23:46:34 +0100

    updated model weights folder name

--- config_utils.py
+++ config_utils.py
@@ -25,14 +25,14 @@ def get_config():
 
 # find the path where we will save the weights
 def get_weights_file_path(config, epoch: str):
-    model_folder = f"{config['datasource']}_{config['model_folder']}"
+    model_folder = f"{config['datasource']}_{config['lang_src']}_{config['lang_tgt']}_{config['model_folder']}"
     model_filename = f"{config['model_basename']}{epoch}.pt"
     return str(Path('.') / model_folder / model_filename)
 
 
 # Find the latest weights file in the weights folder
 def latest_weights_file_path(config):
-    model_folder = f"{config['datasource']}_{config['model_folder']}"
+    model_folder = f"{config['datasource']}_{config['lang_src']}_{config['lang_tgt']}_{config['model_folder']}"
     model_filename = f"{config['model_basename']}*"
     weights_files = list(Path(model_folder).glob(model_filename))
     if len(weights_files) == 0:
